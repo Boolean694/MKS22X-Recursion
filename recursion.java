b107e051e4ed4f2817518898c0c0c27c547d73da
Julian Wong <jwong01@stuy.edu>
2019-01-31 19:56:51 -0500

    write fib method, didn't test

--- recursion.java
+++ recursion.java
@@ -1,16 +1,23 @@
 public class recursion {
-  public double SquareRt(double n) {
+  public static double SquareRt(double n) {
     if(n == 0.0) {return 0.0;}
     double guess = 1.0;
     return closer(n, guess);
   }
-  public double closer(double n, double guess) {
+  public static double closer(double n, double guess) {
     if((guess * guess) / n > 0.9999 && (guess * guess) / n < 1.0001) {
       return guess;
     }
     else {
       return closer(n, (n / guess + guess) / 2);
     }
+  }
+  public static int fib(int n) {
+    return fh(n);
+  }
+  public static int fh(int n) {
+    if(n == 0 || n == 1) {return n;}
+    else {return fh(n - 1) + fh(n - 2);}
   }
   public static void main(String[] args) {
   }

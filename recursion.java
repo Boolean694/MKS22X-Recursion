4993310502a1dd2131e22fa879702ab98b666865
Julian Wong <jwong01@stuy.edu>
2019-03-19 14:31:37 -0400

    asief

--- recursion.java
+++ recursion.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class recursion {
-  public static double SquareRt(double n, double p) {
+  public static double sqrt(double n, double p) {
     if(n == 0.0) {return 0.0;}
     double guess = p;
     return closer(n, guess);
